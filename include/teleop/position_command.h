ca255175c47b4d1d8bb3701e1587ab17456ede2d
Kassra-sinaei <kasra.sinaei@gmail.com>
2024-01-10 18:07:54 -0500

    velocity control tested

--- include/teleop/position_command.h
+++ include/teleop/position_command.h
@@ -7,9 +7,11 @@
 #include "string.h"
 
 #include "rclcpp/rclcpp.hpp"
+#include "chrono"
 
 #include "ros2_unitree_legged_msgs/msg/high_cmd.hpp"
 #include "ros2_unitree_legged_msgs/msg/high_state.hpp"
+#include "unitree_legged_sdk/unitree_legged_sdk.h"
 
 #include "ros2_unitree_legged_msgs/srv/pos_cmd.hpp"
 
@@ -18,7 +20,7 @@ using namespace std;
 
 class PositionCommand : public rclcpp::Node{
     public:
-        PositionCommand(std::string passedNodeName="VOID");
+        PositionCommand(std::string passedNodeName);
         ~PositionCommand();
     private:
         Eigen::Vector3d current_pos_;
@@ -26,6 +28,9 @@ class PositionCommand : public rclcpp::Node{
         Eigen::Vector3d high_bound_;
         rclcpp::Service<ros2_unitree_legged_msgs::srv::PosCmd>::SharedPtr srv_;
         rclcpp::Publisher<ros2_unitree_legged_msgs::msg::HighCmd>::SharedPtr pos_msg_;
+        
+        rclcpp::WallRate::SharedPtr loop_rate_;
+        //rclcpp::executors::SingleThreadedExecutor* executor_;
 
         void send2UDP(const std::shared_ptr<ros2_unitree_legged_msgs::srv::PosCmd::Request> req,
           std::shared_ptr<ros2_unitree_legged_msgs::srv::PosCmd::Response> res);
--- src/position_command.cpp
+++ src/position_command.cpp
@@ -6,6 +6,8 @@ PositionCommand::PositionCommand(std::string passedNodeName):Node(passedNodeName
             std::bind(&PositionCommand::send2UDP, this, std::placeholders::_1, std::placeholders::_2 ));
     pos_msg_ = this->create_publisher<ros2_unitree_legged_msgs::msg::HighCmd>("/high_cmd", 1);
 
+    loop_rate_ = rclcpp::WallRate::make_shared(50);
+
     current_pos_ << 0, 0, 0; 
     low_bound_ << 0, 0, 0; 
     high_bound_ << 0, 0, 0; 
@@ -25,20 +27,40 @@ void PositionCommand::send2UDP(const std::shared_ptr<ros2_unitree_legged_msgs::s
     ros2_unitree_legged_msgs::msg::HighCmd high_cmd_ros;
     high_cmd_ros.head[0] = 0xFE;
     high_cmd_ros.head[1] = 0xEF;
-    // high_cmd_ros.level_flag = HIGHLEVEL;
-    high_cmd_ros.mode = 3;
-    high_cmd_ros.gait_type = 2;
-    high_cmd_ros.speed_level = 1;   // Between 0-2
+    high_cmd_ros.level_flag = UNITREE_LEGGED_SDK::HIGHLEVEL;
+    high_cmd_ros.mode = 2;
+    high_cmd_ros.gait_type = 1;
+    high_cmd_ros.speed_level = 0;   // Between 0-2
     high_cmd_ros.foot_raise_height = 0.1;
-    high_cmd_ros.body_height = 0.4;
+    high_cmd_ros.body_height = 0.1;
     high_cmd_ros.euler[0] = req->phi;
     high_cmd_ros.euler[1] = 0;
     high_cmd_ros.euler[2] = 0;
     high_cmd_ros.position[0] = req->x;
     high_cmd_ros.position[1] = req->y;
+    // high_cmd_ros.velocity[0] = req->x;
+    // high_cmd_ros.velocity[1] = req->y;
     high_cmd_ros.reserve = 0;
+    cout << req->x << endl;
+    if (req->x == 0){
+        res->res = false;
+    }
 
+    double time = 0;
+    chrono::milliseconds max_iter = chrono::milliseconds(2);	
+    while(time < 4 * 2 * 50)
+    {
+        time+=1;
+        pos_msg_->publish(high_cmd_ros);
+        //RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "I'm fine");
+        loop_rate_->sleep();
+        //executor_->spin_some(max_iter);
+    }
+    high_cmd_ros.velocity[0] = 0;
+    high_cmd_ros.velocity[1] = 0;
+    high_cmd_ros.mode = 0;
     pos_msg_->publish(high_cmd_ros);
+
     Eigen::Vector3d temp(req->x, req->y, req->phi);
     this->current_pos_ = this->current_pos_ + temp;
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Position Command sent...");
